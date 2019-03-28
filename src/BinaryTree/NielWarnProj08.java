cfad390df77b3dc6e076f6e54dda032a7ea86d0b
Warner Nielsen <bwn888@gmail.com>
2019-03-27 20:41:47 -0600

    Clean up comments, add method to validate user response

--- src/BinaryTree/NielWarnProj08.java
+++ src/BinaryTree/NielWarnProj08.java
@@ -2,23 +2,53 @@
 package BinaryTree;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class NielWarnProj08 {
 
     public static void main(String[] args) {
-        String rootQuestion = "Does it have legs? (y/n) ";
+        String root1Question = "Does it have legs? ";
+        String root1LeftAnswer = "cat";
+        String root1RightAnswer = "snake";
+        
         String response;
-        String newQuestion;
         Boolean finished = false;
+        
+        Stack<BinaryTree<String>> treeStack = new Stack<>();
 
         BinaryTree<String> tree1 = new BinaryTree();
         
-//        BinaryTree<String> tree2 = new BinaryTree();
-//        BinaryTree<String> tree3;
+        BinaryTree<String> curr = tree1;
 //        
-        tree1.setRootItem(rootQuestion);
-        tree1.attachLeft("cat");
-        tree1.attachRight("snake");
+        tree1.setRootItem(root1Question);
+        tree1.attachLeft(root1LeftAnswer);
+        tree1.attachRight(root1RightAnswer);
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Welcome to the Animal Guessing Computer Learning Game");
+        do {
+            System.out.println("Think of an animal and I will guess it.");
+            System.out.print(tree1.getRootItem());
+            response = input.next();
+            
+            userYesNo(response, input);
+            
+            if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")) {
+                treeStack.push(curr);
+                curr = curr.detachLeftSubtree();
+            } else {
+                treeStack.push(curr);
+                curr = curr.detachRightSubtree();
+            }
+            
+            System.out.print("Continue? ");
+            response = input.next();
+            userYesNo(response, input);
+            
+            finished = true;
+        } while (!finished);
+        
         
         
 //        tree3 = tree1.detachLeftSubtree();
@@ -35,22 +65,17 @@ public class NielWarnProj08 {
 //        System.out.println("Tree1 rightChild: " + tree1.root.rightChild.item);
 //        System.out.println("Tree1 leftChildleftChild: " + tree1.root.leftChild.leftChild.item);
 //        System.out.println("Tree1 leftChildrightChild: " + tree1.root.leftChild.rightChild.item);
-//        
-//        Scanner input = new Scanner(System.in);
-//        
-//        do {
-//            System.out.println("Think of an animal and I will guess it.");
-//            System.out.print(newTree.getRootItem());
-//            response = input.next();
-//            if (response.compareToIgnoreCase("n") == 0) {
-//                System.out.println(questionFirstNo);
-//                
-//            }
-//            finished = true;
-//        } while (!finished);
     }
     
     public static boolean isLeaf(BinaryTree<String> tree1) {
             return tree1.detachLeftSubtree().isEmpty() && tree1.detachRightSubtree().isEmpty();
+    }
+    
+    public static void userYesNo(String response, Scanner input) {
+        while (!(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y") || response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n"))) {
+            System.out.println("Not a valid choice");
+            System.out.print("Please enter 'yes' or 'no': ");
+            response = input.next();
+        }
     }
 }
