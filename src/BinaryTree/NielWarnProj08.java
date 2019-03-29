3b1c5efb851525420216ac3ebce7bb2f54aa596a
warniel08 <bwn888@gmail.com>
2019-03-28 18:01:03 -0600

    Fix attachSubtree, still not working

--- src/BinaryTree/NielWarnProj08.java
+++ src/BinaryTree/NielWarnProj08.java
@@ -32,7 +32,7 @@ public class NielWarnProj08 {
             System.out.print(tree1.getRootItem());
             response = input.nextLine();
             
-            userYesNo(response, input);
+            response = userYesNo(response, input);
             
             // checks yes or no to first question, does it have legs?
             if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")) {
@@ -52,9 +52,10 @@ public class NielWarnProj08 {
                     
                     // insert the new question into the temp tree
                     insertNewQuestion(response, input, curr, temp);
+                    curr = attachSubtree(curr, temp, treeStack);
                 }
                 
-                // while still yes, if curr is a leafNode then guess if single root item
+                // while still yes, if curr is a leafNode then guess if single root item is what it is
                 if (isLeaf(curr)) {
                     System.out.print("Is it a " + curr.getRootItem() + "? " );
                     response = input.nextLine();
@@ -62,12 +63,14 @@ public class NielWarnProj08 {
                     
                     // use insertNewQuestion in case guess is wrong
                     insertNewQuestion(response, input, curr, temp);
+                    curr = attachSubtree(curr, temp, treeStack);
                 }
                 
-                prev = treeStack.pop();
-                prev.attachLeftSubtree(temp);
-                tree1 = prev;
-                curr = tree1;
+//                // assign prev to the binaryTree on the treeStack
+//                prev = treeStack.pop();
+//                // attach the leftSubtree
+//                prev.attachLeftSubtree(temp);
+                
             } else {
                 treeStack.push(curr);
                 curr = curr.detachRightSubtree();
@@ -76,12 +79,18 @@ public class NielWarnProj08 {
                 userYesNo(response, input);
                 
                 insertNewQuestion(response, input, curr, temp);
-                
-                prev = treeStack.pop();
-                prev.attachRightSubtree(temp);
-                tree1 = prev;
-                curr = tree1;
+//                
+//                prev = treeStack.pop();
+//                prev.attachRightSubtree(temp);
+//                tree1 = prev;
+//                curr = tree1;
             }
+            
+            // set tree1 to the item on the treeStack
+            
+            tree1 = curr;
+            // set curr to tree1 to be used in repeat
+//            curr = tree1;
             
             System.out.print("Continue? ");
             response = input.nextLine();
@@ -102,11 +111,22 @@ public class NielWarnProj08 {
      *  then it should attach a left subtree, if not then attach
      *  the tree to the right
      */
-    public static boolean hasRight(BinaryTree<String> curr, BinaryTree<String> temp) {
-        if (curr.detachRightSubtree().isEmpty()) {
-            
+    
+    /*
+     *  method takes a curr binaryTree and a treeStack and checks if subtree of current
+     *  is empty, if so, then it will attach it to the appropriate child
+     */
+    public static BinaryTree<String> attachSubtree(BinaryTree<String> curr, BinaryTree<String> temp, Stack<BinaryTree<String>> treeStack) {
+        BinaryTree<String> stackTree = treeStack.pop();
+        
+        //replace curr with temp
+        if (isLeaf(curr)) {
+            curr.makeEmpty();
+            curr = temp;
+            stackTree.attachLeftSubtree(curr);
+            curr = stackTree;
         }
-        return curr.detachLeftSubtree().isEmpty();
+        return curr;
     }
     
     /*
@@ -126,7 +146,7 @@ public class NielWarnProj08 {
 
             System.out.println("Please type a question whose answer is yes for " + newAnswer +
                     " and no for " + curr.getRootItem() + ":");
-            newQuestion = input.nextLine().toUpperCase();
+            newQuestion = input.nextLine().toUpperCase() + " ";
 
             temp.setRootItem(newQuestion);
             temp.attachRight(curr.getRootItem());
@@ -134,12 +154,14 @@ public class NielWarnProj08 {
         }
     }
     
-    public static void userYesNo(String response, Scanner input) {
+    public static String userYesNo(String response, Scanner input) {
         while (!(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y") || 
                 response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n"))) {
             System.out.println("Not a valid choice");
             System.out.print("Please enter 'yes' or 'no': ");
             response = input.nextLine();
+            return response;
         }
+        return response;
     }
 }
